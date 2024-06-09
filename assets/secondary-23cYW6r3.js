3ab21992e1e1277e7088ccb2c64f1f804bc7b443
graganold <geordinraganold@gmail.com>
2024-06-08 23:13:42 -0400

    Updates

--- assets/secondary-23cYW6r3.js
+++
@@ -1,1 +0,0 @@
-import{R as t,j as e}from"./index-8uvJLHDF.js";const o=App;t.createRoot(document.getElementById("root2")).render(e.jsx(o,{}));
---
+++ secondary.js
@@ -0,0 +1,1 @@
+import{R as t,j as e}from"./index.js";const o=App;t.createRoot(document.getElementById("root2")).render(e.jsx(o,{}));
